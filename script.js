9ef30ecf06d320a3b9edde6d737500c6a8d083a9
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-12 20:37:40 +0200

    Completed.

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ for (let i = 0; i < squares.length; i++) {
   const square = squares[i];
   square.addEventListener("click", () => {
     // alert("You clicked " + i);
-    if (squares[i + 7].classList.contains("taken")) {
+    if (
+      squares[i + 7].classList.contains("taken") &&
+      !squares[i].classList.contains("taken")
+    ) {
       if (currentPlayer == 1) {
         squares[i].classList.add("taken");
         squares[i].classList.add("playerOne");
@@ -22,5 +25,80 @@ for (let i = 0; i < squares.length; i++) {
     } else {
       alert("Can't place your token here");
     }
+
+    checkBoard();
   });
+}
+
+function checkBoard() {
+  for (let i = 0; i < squares.length; i++) {
+    const square = squares[i];
+    switch (checkToken(i)) {
+      case 1:
+        resultDisplay.innerHTML = "Player one wins!";
+        break;
+
+      case 2:
+        resultDisplay.innerHTML = "Player two wins!";
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+function checkToken(index) {
+  let victory = true;
+
+  // checking for player one
+  for (let i = 0; i < 4; i++) {
+    if (index + i < squares.length) {
+      if (!squares[index + i].classList.contains("playerOne")) {
+        victory = false;
+      }
+    }
+  }
+  if (victory) {
+    return 1;
+  }
+  victory = true;
+
+  for (let i = 0; i < 4; i++) {
+    if (index + i * 7 < squares.length) {
+      if (!squares[index + i * 7].classList.contains("playerOne")) {
+        victory = false;
+      }
+    }
+  }
+  if (victory) {
+    return 1;
+  }
+
+  victory = true;
+  // checking for player two
+  for (let i = 0; i < 4; i++) {
+    if (index + i < squares.length) {
+      if (!squares[index + i].classList.contains("playerTwo")) {
+        victory = false;
+      }
+    }
+  }
+  if (victory) {
+    return 2;
+  }
+  victory = true;
+
+  for (let i = 0; i < 4; i++) {
+    if (index + i * 7 < squares.length) {
+      if (!squares[index + i * 7].classList.contains("playerTwo")) {
+        victory = false;
+      }
+    }
+  }
+  if (victory) {
+    return 2;
+  }
+
+  return false;
 }
