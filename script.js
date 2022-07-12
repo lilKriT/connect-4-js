bb3f216fe43821494106880ff01b3ea7cb8d54f8
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-12 18:31:22 +0200

    Placing tokens.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const squares = document.querySelectorAll(".grid > div");
+const resultDisplay = document.querySelector("#result");
+const currentPlayerDisplay = document.querySelector("#current-player");
+
+let currentPlayer = 1;
+for (let i = 0; i < squares.length; i++) {
+  const square = squares[i];
+  square.addEventListener("click", () => {
+    // alert("You clicked " + i);
+    if (squares[i + 7].classList.contains("taken")) {
+      if (currentPlayer == 1) {
+        squares[i].classList.add("taken");
+        squares[i].classList.add("playerOne");
+        currentPlayer = 2;
+        currentPlayerDisplay.innerHTML = currentPlayer;
+      } else if (currentPlayer == 2) {
+        squares[i].classList.add("taken");
+        squares[i].classList.add("playerTwo");
+        currentPlayer = 1;
+        currentPlayerDisplay.innerHTML = currentPlayer;
+      }
+    } else {
+      alert("Can't place your token here");
+    }
+  });
+}
